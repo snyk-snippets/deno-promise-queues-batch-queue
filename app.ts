72b2a6929335bb02aca7fdd0d760fb93815eed21
Liran Tal <liran.tal@gmail.com>
2023-11-14 10:27:24 +0000

    feat: add a CLI progres bar

--- app.ts
+++ app.ts
@@ -1,4 +1,5 @@
 import { BatchQueue } from "https://deno.land/x/batch_queue/mod.ts";
+import ProgressBar from "https://deno.land/x/progress/mod.ts";
 
 // URLs to make HTTP requests to
 const urls = [
@@ -7,22 +8,28 @@ const urls = [
   'https://registry.npmjs.org/package3'
 ];
 
+const totalRequests = urls.length;
+let currentRequest = 0;
+const progressBar = new ProgressBar({
+  total: totalRequests,
+  complete: '=',
+  incomplete: ' ',
+  display: ':bar :percent',
+});
+
 // Initialize the queue with a concurrency of 2
 const q = new BatchQueue(2);
 
 // Function to make HTTP request
 async function makeHttpRequest(url) {
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log(data);
-  } catch (error) {
-    console.error('Error fetching URL:', url, error);
-  }
+  progressBar.render(++currentRequest);
+  const response = await fetch(url);
+  const data = await response.json();
+  // @TODO do something with the data...
 }
 
 for (const url of urls) {
-  q.queue(() => makeHttpRequest(url));
+	q.queue(() => makeHttpRequest(url));
 }
 
 await q.run()
